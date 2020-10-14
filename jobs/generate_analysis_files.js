54b4be122aafd5a64d1cede16d97a6ad5850edf3
Chad Tomkiss <chad.tomkiss@madetech.com>
2020-10-14 12:10:42 +0100

    Tidy up job file

--- jobs/generate_analysis_files.js
+++ jobs/generate_analysis_files.js
@@ -7,31 +7,38 @@ const generateLeaversReport = require('../src/reports/leavers');
 const { refreshViews } = require('../src/reports/views');
 const { uploadFile } = require('../src/reports/s3');
 
+const finish = async (dest) => {
+  const now = dayjs();
+  const zipName = `analysis_files_${now.format('YYYY_MM_DD_HH_mm_ss')}.zip`;
+
+  await exec(`cd ${dest} && zip -r ${zipName} *.csv`);
+
+  await uploadFile('sfcreports', zipName, fs.readFileSync(`${dest}/${zipName}`));
+
+  console.log(`Cleaning up ${dest}`);
+  await exec(`rm -rf ${dest}`);
+}
+
+const run = async () => {
+  console.log(dayjs().format('DD-MM-YYYY HH:mm:ss'));
+
+  const dest = './output';
+  await exec(`mkdir -p ${dest}`);
+
+  await refreshViews();
+
+  const runDate = dayjs().format('DD-MM-YYYY');
+  
+  await generateWorkplaceReport(runDate, dest);
+  await generateWorkersReport(runDate, dest);
+  await generateLeaversReport(runDate, dest);
+
+  await finish();
+
+  console.log(dayjs().format('DD-MM-YYYY HH:mm:ss'));
+}
+
 (async () => {
-  async function run() {
-    const now = dayjs();
-    const runDate = now.format('DD-MM-YYYY');
-
-    const dest = './output';
-    const zipName = `analysis_files_${now.format('YYYY_MM_DD_HH_mm_ss')}.zip`;
-
-    await exec(`mkdir -p ${dest}`);
-
-    console.log(dayjs().format('DD-MM-YYYY HH:mm:ss'));
-
-    await refreshViews();
-
-    await generateWorkplaceReport(runDate, dest);
-    await generateWorkersReport(runDate, dest);
-    await generateLeaversReport(runDate, dest);
-
-    await exec(`cd ${dest} && zip -r ${zipName} *.csv`);
-
-    await uploadFile('sfcreports', zipName, fs.readFileSync(`${dest}/${zipName}`));
-
-    console.log(dayjs().format('DD-MM-YYYY HH:mm:ss'));
-  }
-
   run()
     .then(() => {
       process.exit(0);
