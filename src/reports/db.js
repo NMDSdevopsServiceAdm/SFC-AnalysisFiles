e2c460d0b26447d09b5a5f8815d55fb608bea403
Chad Tomkiss <chad.tomkiss@madetech.com>
2020-11-01 13:11:11 +0000

    Target public schema as well as CQC schema to use postgis extensions

--- src/reports/db.js
+++ src/reports/db.js
@@ -3,7 +3,7 @@ const config = require('../../config');
 const pg = require('knex')({
   client: 'pg',
   connection: config.get('db.url'),
-  searchPath: 'cqc',
+  searchPath: ['public', 'cqc'],
 });
 
 module.exports = pg;
