d076b17c949aa607a03520dfdb4dbad898c3cb34
Richard Pentecost <richard.pentecost@madetech.com>
2023-05-16 13:59:15 +0100

    add default environment to config file, change update benchmarks to use new update benchmarks function

--- jobs/update_benchmarks.js
+++ jobs/update_benchmarks.js
@@ -4,6 +4,8 @@ const exec = require('await-exec');
 const config = require('../config');
 const { getBenchmarksFiles } = require('../src/utils/s3/benchmarks');
 const { updateBenchmarksDbTables } = require('../src/benchmarks/update_benchmarks_db_tables');
+const { updateNewBenchmarksDbTables } = require('../src/benchmarks/update_new_benchmarks_db_tables');
+
 const slack = require('../src/utils/slack/slack-logger');
 
 const reportDir = './benchmarkFiles';
@@ -11,28 +13,30 @@ const s3 = new AWS.S3();
 
 const setup = async () => {
   console.log(`Refreshing ${reportDir} directory`);
-  
+
   await exec(`rm -rf ${reportDir}`);
   await exec(`mkdir ${reportDir}`);
 };
 
 const downloadAllFilesFromS3 = async (benchmarksFiles) => {
-  console.log('downloading from s3')
+  console.log('downloading from s3');
   const bucket = config.get('s3.benchmarksBucket');
 
-  const reportsObj = {}
-  await Promise.all(benchmarksFiles.map(async (file) => {
-    const params = {
-      Bucket: bucket,
-      Key: file.Key
-    };
+  const reportsObj = {};
+  await Promise.all(
+    benchmarksFiles.map(async (file) => {
+      const params = {
+        Bucket: bucket,
+        Key: file.Key,
+      };
 
-    const downloadedReport = await s3.getObject(params).promise();
-    reportsObj[file.Key] = downloadedReport.Body.toString();
-  }));
+      const downloadedReport = await s3.getObject(params).promise();
+      reportsObj[file.Key] = downloadedReport.Body.toString();
+    }),
+  );
 
   return reportsObj;
-}
+};
 
 const updateBenchmarks = async () => {
   const benchmarksFiles = await getBenchmarksFiles();
@@ -42,39 +46,36 @@ const updateBenchmarks = async () => {
   if (benchmarksToBeUpdated) {
     await setup();
     const reports = await downloadAllFilesFromS3(benchmarksFiles);
-    await updateBenchmarksDbTables(reports);
+    // await updateBenchmarksDbTables(reports);
+    await updateNewBenchmarksDbTables(reports);
 
-    console.log(`${dayjs()}: Files successfully updated`);
-    await sendSlackBenchmarksUpdatedMessage(`${dayjs()}: Benchmarks successfully updated`);
+    // console.log(`${dayjs()}: Files successfully updated`);
+    // await sendSlackBenchmarksUpdatedMessage(`${dayjs()}: Benchmarks successfully updated`);
     return;
   } else {
     console.log(`${dayjs()}: Files not updated since ${dayjs().subtract(1, 'day')}`);
-    await sendSlackBenchmarksUpdatedMessage(`${dayjs()}: Benchmarks not updated since ${dayjs().subtract(1, 'day')}`);
+    // await sendSlackBenchmarksUpdatedMessage(`${dayjs()}: Benchmarks not updated since ${dayjs().subtract(1, 'day')}`);
     return;
   }
 };
 
 const updatedSinceYesterday = (files) => {
-  return files.some(file => {
+  return files.some((file) => {
     return dayjs(file.LastModified) >= dayjs().subtract(1, 'day');
   });
-}
+};
 
 const sendSlackBenchmarksUpdatedMessage = async (message) => {
-  await slack.info(
-    `${config.get('db.name')} - Update Benchmarks`,
-    message,
-    'slack.benchmarksUrl'
-  );
-} 
+  await slack.info(`${config.get('db.name')} - Update Benchmarks`, message, 'slack.benchmarksUrl');
+};
 
 const sendSlackBenchmarksErrorMessage = async (errorMessage) => {
   await slack.error(
-    `${config.get('db.name')} - Update Benchmarks`, 
-    `There was an error updating Benchmarks \n ${errorMessage}`, 
-    'slack.benchmarksUrl'
-    );
-} 
+    `${config.get('db.name')} - Update Benchmarks`,
+    `There was an error updating Benchmarks \n ${errorMessage}`,
+    'slack.benchmarksUrl',
+  );
+};
 
 (async () => {
   updateBenchmarks()
