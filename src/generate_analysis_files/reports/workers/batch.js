69a05fdd6ef35c2478ff808e68cf963c59edf640
jonathan.hardy <Jonathan.Hardy97@gmail.com>
2024-05-20 12:09:32 +0100

    fixde typo

--- src/generate_analysis_files/reports/workers/batch.js
+++ src/generate_analysis_files/reports/workers/batch.js
@@ -636,7 +636,7 @@ const findWorkersByBatch = (batchNum) =>
                     WHEN 'Deputy manager' THEN 43
                     WHEN 'Learning and development lead' THEN 44
                     WHEN 'Team leader' THEN 45
-                    WHEN 'Data nalyst' THEN 46
+                    WHEN 'Data analyst' THEN 46
                     WHEN 'Data governance manager' THEN 47
                     WHEN 'IT and digital support' THEN 48
                     WHEN 'IT manager' THEN 49
