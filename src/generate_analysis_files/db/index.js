de963145c950660ffa153385048401d3b4bf852d
Zuhal-Ayob <zuhal.ayob@skillsforcare.org.uk>
2025-11-19 14:26:38 +0000

    add console log

--- src/generate_analysis_files/db/index.js
+++ src/generate_analysis_files/db/index.js
@@ -2,10 +2,12 @@ const config = require('../../../config');
 const fs = require('fs');
 
 
+console.log("CA exists:", config.get("db.sslCAPath"), fs.existsSync(config.get("db.sslCAPath")));
+
+
 const pg = require('knex')({
   client: 'pg',
   connection: {connectionString: config.get('db.url'),
-
       ssl: {
       rejectUnauthorized: true,
       ca: fs.readFileSync(config.get('db.sslCAPath'), 'utf8'),
