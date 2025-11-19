a93d6862d50f01ceb258eb93f110cd106f437308
Zuhal-Ayob <zuhal.ayob@skillsforcare.org.uk>
2025-11-19 15:52:04 +0000

    Add the ssl to the cqc model

--- src/generate_analysis_files/db/index.js
+++ src/generate_analysis_files/db/index.js
@@ -1,10 +1,6 @@
 const config = require('../../../config');
 const fs = require('fs');
 
-
-console.log("CA exists:", config.get("db.sslCAPath"), fs.existsSync(config.get("db.sslCAPath")));
-
-
 const pg = require('knex')({
   client: 'pg',
   connection: {connectionString: config.get('db.url'),
