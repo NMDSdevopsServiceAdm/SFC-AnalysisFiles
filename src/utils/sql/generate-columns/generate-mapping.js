db3163e0b55ae44e533e20f05eeb0e1bc80fb565
Zuhal-Ayob <zuhal.ayob@skillsforcare.org.uk>
2026-06-18 17:24:59 +0100

    Add more mapping

--- src/utils/sql/generate-columns/generate-mapping.js
+++ src/utils/sql/generate-columns/generate-mapping.js
@@ -10,6 +10,24 @@ const NEW_YESNO_TYPE_MAPPING = {
   "Don''t know": -2,
 };
 
+
+const YESNO_TYPE_MAPPING = {
+  No: 0,
+  Yes: 1,
+  "Don''t know": 2,
+};
+
+const TRAINING_DELIVERY_MAPPING = {
+  'In-house staff': 1,
+  'External provider': 2,
+};
+
+const TRAINING_DELIVERY_TYPE_MAPPING = {
+  'Face to face': 1,
+  'E-learning': 2,
+};
+
+
 const RATE_TYPE_MAPPING = {
   'Hourly rate': 1,
   'Flat rate': 2,
@@ -20,4 +38,7 @@ module.exports = {
   OLD_YESNO_TYPE_MAPPING,
   NEW_YESNO_TYPE_MAPPING,
   RATE_TYPE_MAPPING,
+  YESNO_TYPE_MAPPING,
+  TRAINING_DELIVERY_MAPPING,
+  TRAINING_DELIVERY_TYPE_MAPPING
 };
