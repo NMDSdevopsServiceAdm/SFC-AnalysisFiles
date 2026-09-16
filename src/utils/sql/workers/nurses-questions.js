74b2cf98564f9a24c00b183756a0cf5c16d82cdb
Zuhal-Ayob <zuhal.ayob@skillsforcare.org.uk>
2026-09-16 16:49:58 +0100

    change the query to show -1 when there is no answer and is a registered nurse

--- src/utils/sql/workers/nurses-questions.js
+++ src/utils/sql/workers/nurses-questions.js
@@ -2,10 +2,21 @@
 
 const nursesQuestionAnswersColumn = (id, nursesQuestionAnswersCode) => {
   return (
-    '(SELECT "AnalysisFileCode" FROM "NurseFieldOfPractice" n ' +
-    'JOIN "WorkerNurseFieldsOfPractice" wn on n."ID" = wn."NurseFieldOfPracticeID" ' +
-    `WHERE w."ID" = wn."WorkerID" AND "NurseFieldOfPracticeID"= ${id} LIMIT 1) ${nursesQuestionAnswersCode},`
-
+    `CASE ` +
+    `WHEN EXISTS (` +
+      `SELECT 1 ` +
+      `FROM "WorkerNurseFieldsOfPractice" wn ` +
+      `WHERE wn."WorkerID" = w."ID" ` +
+      `AND wn."NurseFieldOfPracticeID" = ${id}` +
+    `) THEN 1 ` +
+    `WHEN w."MainJobFKValue" = 23 ` +
+      `AND NOT EXISTS (` +
+        `SELECT 1 ` +
+        `FROM "WorkerNurseFieldsOfPractice" wn ` +
+        `WHERE wn."WorkerID" = w."ID"` +
+      `) THEN -1 ` +
+    `ELSE 0 ` +
+    `END ${nursesQuestionAnswersCode},`
   );
 };
 
